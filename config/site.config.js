d3ea3206bdfe4c48bb8f0cfb9d0fa72369de5ef9
XILEJ <75837117+XILEJ@users.noreply.github.com>
2022-03-16 20:30:24 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -35,7 +35,9 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
    footer:
-    'Powered by <a href="https://github.com/spencerwooo/onedrive-vercel-index" https://img.shields.io/badge/onedrive--vercel--index-github-black</a>. Made with ❤ by SpencerWoo.',
+    '<img src="https://img.shields.io/badge/OneDrive-2C68C3?style=flat&logo=microsoft-onedrive&logoColor=white" alt="OneDrive" />
+  <img src="https://img.shields.io/badge/Next.js-black?style=flat&logo=next.js&logoColor=white" alt="Next.js" />
+  <img src="https://img.shields.io/badge/Vercel-black?style=flat&logo=Vercel&logoColor=white" alt="Vercel" />',
   
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
