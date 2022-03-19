b569953db5f08071d9135baa3dd63734e4303302
XILEJ <75837117+XILEJ@users.noreply.github.com>
2022-03-19 08:40:53 +0800

    Update next-i18next.config.js

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,8 +2,8 @@ const path = require('path')
 
 module.exports = {
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'zh-CN']
+    defaultLocale: 'zh-CN',
+    locales: ['zh-CN']
   },
   localePath: path.resolve('public/locales'),
   reloadOnPrerender: process.env.NODE_ENV === 'development',
